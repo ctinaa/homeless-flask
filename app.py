37d9385e8407253014517dea2a43081336825bde
Chi Wong <cwong159@gmail.com>
2015-04-05 08:04:00 -0700

    SQLite Try

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ DATABASE = 'app.db'
 # with app.app_context():
 # 	get_db()
 
-db = sqlite3
+db = sqlite3()
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
