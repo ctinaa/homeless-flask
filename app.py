4ffb44c9f8e75058c26feff2993ce7310c326973
ctinaa <christina.fcook@yahoo.com>
2015-04-04 22:51:09 -0700

    adding login on page

--- app.py
+++ app.py
@@ -11,6 +11,15 @@ def home():
 @app.route('/signin')
 def signin():
 	return render_template('signin.html')
-	
+
+@app.route('/signon')
+def signin():
+	return render_template('signon.html')
+
+
+@app.route('/profile')
+def signin():
+	return render_template('profile.html')
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
