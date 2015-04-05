007b9462e073ea19067c7f70d961c093e4fd15b5
Chi Wong <cwong159@gmail.com>
2015-04-04 20:32:29 -0700

    Facebook Data Return Test

--- app.py
+++ app.py
@@ -1,16 +1,67 @@
 #!/usr/bin/python 
 
-from flask import * 
+# from flask import * 
 
-app = Flask(__name__) 
+# app = Flask(__name__) 
 
-@app.route('/')
-def home(): 
-	return render_template('index.html')
+# @app.route('/')
+# def home(): 
+# 	return render_template('index.html')
 
-@app.route('/login')
-def login():
-	return render_template('login2.html')
+# @app.route('/login')
+# def login():
+# 	return render_template('login2.html')
 
+#----------------------------------------
+# facebook authentication
+#----------------------------------------
+
+from flask import url_for, request, session, redirect
+from flask_oauth import OAuth
+
+FACEBOOK_APP_ID = '000000000000000'
+FACEBOOK_APP_SECRET = '0a0b0c00000000000000000000x0y0z0'
+
+oauth = OAuth()
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': ('email, ')}
+)
+
+@facebook.tokengetter
+def get_facebook_token():
+    return session.get('facebook_token')
+
+def pop_login_session():
+    session.pop('logged_in', None)
+    session.pop('facebook_token', None)
+
+@app.route("/facebook_login")
+def facebook_login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next'), _external=True))
+
+@app.route("/facebook_authorized")
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None or 'access_token' not in resp:
+        return redirect(next_url)
+
+    session['logged_in'] = True
+    session['facebook_token'] = (resp['access_token'], '')
+
+    return redirect(next_url)
+
+@app.route("/logout")
+def logout():
+    pop_login_session()
+    return redirect(url_for('index'))
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
